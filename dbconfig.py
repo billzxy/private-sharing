19d3e526a59701df1c6295227ff2f9d7e7896fdb
Xiaoyan Zhang <billzxy1214@hotmail.com>
2017-11-28 23:48:16 +0800

    DB config file is now standalone

--- dbconfig.py
+++ dbconfig.py
@@ -1,9 +1,18 @@
 import pymysql
+import os
+
+def getConfig():
+    values = []
+    with open( os.path.join(os.path.dirname(__file__), "dbconfig.txt") )as f:
+        for line in f:
+            values.append(line.rstrip())
+    return values
 
 def getConnection():
-	return pymysql.connect(host='localhost',
-                       user='root',		#change database username here 	
-                       password='billzxy', #Change your password here
-                       db='pricosha', #Change the local server database here 
+    values = getConfig()
+    return pymysql.connect(host='localhost',
+                       user=values[0],		#change database username here
+                       password=values[1], #Change your password here
+                       db=values[2], #Change the local server database here
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
